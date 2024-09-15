9822954fdf06c934f525f363cf5e0cc88d7a19c8
Razan <94683287+razanasb@users.noreply.github.com>
2024-09-15 13:37:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,28 +1,38 @@
 import gradio as gr
 from transformers import pipeline
 
-# Load pre-trained sentiment analysis model
+# Load the sentiment analysis pipeline
 sentiment_analysis = pipeline("sentiment-analysis", model="distilbert/distilbert-base-uncased-finetuned-sst-2-english")
 
-# analyze user's mood from text
+# Function to analyze mood
 def analyze_mood(user_input):
-    result = sentiment_analysis(user_input)[0]
-    
-    # assign mood based on sentiment
-    if result["label"] == "POSITIVE":
-        mood = "Happy"
-        suggestion = "Keep enjoying your day 😊"
-    elif result["label"] == "NEGATIVE":
-        mood = "Sad"
-        suggestion = "Try playing a game you like or practice some deep breathing exercises it might help!🍃"
+    # analyze mood from text
+    results = sentiment_analysis(user_input)
+    mood_summary = {"POSITIVE": 0, "NEGATIVE": 0, "NEUTRAL": 0}
+    suggestions = []
+
+    # sum up scores
+    for result in results:
+        label = result["label"]
+        score = result["score"]
+        mood_summary[label] += score
+
+    # find most mood
+    main_mood = max(mood_summary, key=mood_summary.get)
+
+    # suggest based on mood
+    if main_mood == "POSITIVE":
+        suggestion = "Keep enjoying your day :)"
+    elif main_mood == "NEGATIVE":
+        suggestion = "Maybe play a game or breathe deeply could help!"
     else:
-        mood = "Neutral"
-        suggestion = "You're doing well! Stay calm 🌸"
-    
-    # Output mood and suggestion
-    return "Your mood is " + mood + ". " + suggestion
+        suggestion = "Doing well! stay calm"
 
-inputs = gr.Textbox(label="How are you feeling today?", placeholder="Type your thoughts here...")
+    # return mood and suggestion
+    return "Your mood seems mostly " + main_mood.lower() + ". " + suggestion
+
+inputs = gr.Textbox(label="How are you today?", placeholder="Type your feelings here...")
 outputs = gr.Textbox(label="Mood and Suggestion")
+interface = gr.Interface(fn=analyze_mood, inputs=inputs, outputs=outputs, title="Mood Analyzer with Suggestions")
 
-gr.Interface(fn=analyze_mood, inputs=inputs, outputs=outputs, title="Mood Analyzer with Suggestions").launch()
+interface.launch()
